6fab0826af599d8b4088e22960d2d3098f89836a
James McGrogan <jmcgroga@gmail.com>
2018-02-10 21:43:58 -0500

    Updated servicenow-background-script.el.  Added servicenow-run-script-kill-script-background, servicenow-kill-script-background, servicenow-kill-all-script-background.  Updated servicenow--run-script to check for valid script output.  Updated servicenow--background-script-link-line-number to check for empty buffer.

--- servicenow-background-script.el
+++ servicenow-background-script.el
@@ -7,6 +7,12 @@
     (progn
       (servicenow-login '(lambda () (servicenow--run-script))))))
 
+;;;###autoload
+(defun servicenow-run-script-kill-script-background ()
+  (interactive)
+  (servicenow-kill-script-background)
+  (servicenow-run-script))
+
 (defun servicenow--run-script ()
   (let ((bufcontent (buffer-string))
         (origbufname (buffer-name)))
@@ -24,17 +30,21 @@
                (lambda (&key data &allow-other-keys)
                  (let* ((scriptbufname (buffer-name))
                         (bufname (servicenow--background-script-output-buffer-name scriptbufname))
-                        (origbufname (buffer-name)))
-                   (message "Finished executing %s" origbufname)
-                   (servicenow--split-window-apply-function bufname
-                                                            `(lambda ()
-                                                               (mapcar 'insert (servicenow--get-script-output data))
-                                                               (insert "\n")
-                                                               (funcall 'servicenow-backgroundscriptoutputmode)
-                                                               (servicenow--background-script-link-line-number (buffer-name) ,scriptbufname)
-                                                               (servicenow--background-script-link-line-number-caused-by (buffer-name) ,scriptbufname)
-                                                               (read-only-mode)
-                                                               (goto-line 1))))))
+                        (origbufname (buffer-name))
+                        (scriptoutput (servicenow--get-script-output data)))
+                   (if (car scriptoutput)
+                       (progn
+                         (message "Finished executing %s" origbufname)
+                         (servicenow--split-window-apply-function bufname
+                                                                  `(lambda ()
+                                                                     (mapcar 'insert scriptoutput)
+                                                                     (insert "\n")
+                                                                     (funcall 'servicenow-backgroundscriptoutputmode)
+                                                                     (servicenow--background-script-link-line-number (buffer-name) ,scriptbufname)
+                                                                     (servicenow--background-script-link-line-number-caused-by (buffer-name) ,scriptbufname)
+                                                                     (read-only-mode)
+                                                                     (goto-line 1))))
+                     (message "Script did not execute!")))))
      :error (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
                            (message "Error: %S" error-thrown))))))
 
@@ -47,7 +57,7 @@
       (goto-char (point-min))
       (let ((linelength (- (line-end-position)
                            (line-beginning-position))))
-        (if (re-search-forward "; \\(line \\([0-9]+\\)\\))" linelength t)
+        (if (and (> linelength 0) (re-search-forward "; \\(line \\([0-9]+\\)\\))" linelength t))
             (progn
               (let* ((matchbeg (match-beginning 1))
                      (matchstr (match-string 1))
@@ -79,4 +89,19 @@
                                       (with-current-buffer ,scriptbufname
                                         (switch-to-buffer-other-window ,scriptbufname)
                                         (goto-line ,linum))))))))))
+
+;;;###autoload
+(defun servicenow-kill-script-background ()
+  (interactive)
+  (servicenow--kill-script-background (servicenow--background-script-output-buffer-name (buffer-name))))
+
+;;;###autoload
+(defun servicenow-kill-all-script-background ()
+  (interactive)
+  (servicenow--kill-script-background "Script - Background ("))
+
+(defun servicenow--kill-script-background (prefix)
+  (dolist (buf (buffer-list))
+    (if (string-prefix-p prefix (buffer-name buf))
+        (kill-buffer buf))))
 
